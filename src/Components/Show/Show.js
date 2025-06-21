cd19457aedd0952b9e2be2f009a99d0750001393
nandini <jitcse.ndeshpande2001@gmail.com>
2025-06-21 13:46:59 +0530

    commit stic bar

--- src/Components/Show/Show.js
+++ src/Components/Show/Show.js
@@ -53,7 +53,8 @@ export default function Show() {
         className="container-fluid text-center"
         style={{ marginTop: "100px" }}
       >
-        <div className="d-flex mt-3 mb-4 ">
+     <div style={{zIndex:"10",top:"90px",position:"fixed",left:"0",right:"0", backgroundColor: "var(--background-color)"}}>
+         <div className="d-flex mt-3 mb-4 ">
           <div
             className="card p-1"
             style={{
@@ -78,44 +79,45 @@ export default function Show() {
             </h6>
           </div>
         </div>
-        <div className="row">
+     </div>
+        <div className="row" style={{marginTop:"160px"}}>
           <div className="col-12">
             <table className="table">
               <thead>
-                <tr>
+                <tr style={{}}>
                   <th
                     scope="col"
-                    style={{
+                    style={{ 
                       backgroundColor: "var(--background-color)",
-                      color: "var(--text-color)",
+                      color: "var(--text-color)",position:"sticky",zIndex:"1",top:"160px"
                     }}
                   >
                     Sr No.
                   </th>
-                  <th scope="col" className="bg-primary text-white">
+                  <th scope="col" className="bg-primary text-white"style={{position:"sticky",zIndex:"1",top:"160px"}}>
                     <CiTrophy /> Sc.
                   </th>
 
-                  <th scope="col-2" className="bg-warning text-white">
+                  <th scope="col-2" className="bg-warning text-white" style={{position:"sticky",zIndex:"1",top:"160px"}}>
                     <CiLight /> Ac.
                   </th>
 
-                  <th scope="col-2" className="bg-info text-white">
+                  <th scope="col-2" className="bg-info text-white" style={{position:"sticky",zIndex:"1",top:"160px"}}>
                     <PiNotepadLight /> At.
                   </th>
 
-                  <th scope="col" className="bg-success text-white">
+                  <th scope="col" className="bg-success text-white" style={{position:"sticky",zIndex:"1",top:"160px"}}>
                     <GrStatusGood /> Co.
                   </th>
 
-                  <th scope="col" className="bg-danger text-white">
+                  <th scope="col" className="bg-danger text-white" style={{position:"sticky",zIndex:"1",top:"160px"}}>
                     <RxCross2 /> Inc.
                   </th>
 
-                  <th scope="col" className="bg-secondary text-white">
+                  <th scope="col" className="bg-secondary text-white" style={{position:"sticky",zIndex:"1",top:"160px"}}>
                     <FaRegQuestionCircle /> Unat.
                   </th>
-                  <th scope="col" className=" text-white" style={{backgroundColor:"purple"}}>
+                  <th scope="col" className=" text-white" style={{backgroundColor:"purple",position:"sticky",zIndex:"1",top:"160px"}}>
                    <BiCalendar /> Date
                   </th>
                 </tr>
